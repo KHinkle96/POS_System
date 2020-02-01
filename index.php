6344c920cc485e77dabde66647d2ec03aafe8c17
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-01-31 18:49:49 -0800

    made 9 buttons in php

--- index.php
+++ index.php
@@ -3,3 +3,17 @@
 <title>POS System</title>
 <h1> POS System <h1>
 </head>
+
+<body>
+
+<span>
+<button type="button" value="0">0</button>
+<button type="button" value="1">1</button>
+<button type="button" value="2">2</button>
+<button type="button" value="3">3</button>
+<button type="button" value="4">4</button>
+<button type="button" value="5">5</button>
+<button type="button" value="6">6</button>
+<button type="button" value="7">7</button>
+<button type="button" value="8">8</button>
+<button type="button" value="9">9</button>
