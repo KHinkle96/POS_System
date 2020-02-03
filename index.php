5998e00c117160b8c524aa5fe6676ad6424b9889
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-02-02 16:46:32 -0800

    updated the buttons

--- index.php
+++ index.php
@@ -1,28 +1,24 @@
+<!DOCTYPE html>
 <html>
 <head>
-<title>POS System</title>
+<link rel="stylesheet" type="text/css" href="app.css">
+<title> POS System </title>
 <h1> POS System <h1>
 </head>
 
 <body>
-<span>
+<button id = “btn0”>0</button>
+<button id = “btn1”>1</button>
+<button id = “btn2”>2</button>
+<button id = “btn3”>3</button>
+<button id = “btn4”>4</button>
+<button id = “btn5”>5</button>
+<button id = “btn6”>6</button>
+<button id = “btn7”>7</button>
+<button id = “btn8”>8</button>
+<button id = “btn9”>9</button>
 
-<button type="button" value="0">0</button>
-<button type="button" value="1">1</button>
-<button type="button" value="2">2</button>
-<button type="button" value="3">3</button>
-<button type="button" value="4">4</button>
-<button type="button" value="5">5</button>
-<button type="button" value="6">6</button>
-<button type="button" value="7">7</button>
-<button type="button" value="8">8</button>
-<button type="button" value="9">9</button>
-<button type="submit" value="submit">Add Amount</button>
-
-<span>
-
-<div>
-
-<button type="submit" value="submit">Save Till</button>
-
-<div>
+<span></span>
+<form action = "/" method = "post">
+    <input type = "submit" value = "Send" />
+</form>
